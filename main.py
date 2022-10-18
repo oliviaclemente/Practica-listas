873ac1317c90ac41d695e1d33971d7ca498d0a16
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-18 10:39:22 +0000

    CAMBIO

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-def ejercicio 1()
+def ejercicio1():
+    lista = ["P", "t"]
+    # TODO
+    assert "".join(lista) == "Python"
+
+    #ejemplo de assert
+    
+
+def ejercicio2():
+    lista = [1, 4, 2, 5, 4, 3, 4, 7, 5, 8, 9]
+    # TODO
+    assert lista == list(range(1, 6))
+
