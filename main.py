40927225018a77b4f0b77bed2f127d0499a5c4c9
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-18 11:38:12 +0000

    commit

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-def ejercicio1():
-    lista = ["P", "t"]
-    # TODO
-    assert "".join(lista) == "Python"
+# Ejercicio 1: Utilice las funciones anteriores para agregar elementos faltantes
+# Ejercicio 2: Utilice las funciones anteriores para elminar elementos sobrantes
 
-    #ejemplo de assert
-    
+def ejercicio1():      
+  lista = ["P", "t"]      
+  # TODO       
+  assert "".join(lista) == "Python"   
+  lista.insert(1,"y")
+  lista.pop("h","o","n")  
 
 def ejercicio2():
     lista = [1, 4, 2, 5, 4, 3, 4, 7, 5, 8, 9]
     # TODO
     assert lista == list(range(1, 6))
-
+    lista.remove(4)
+    lista.remove(5)
+    lista.remove(4)
+    lista.remove(7)
+    lista.remove(8)
+    lista.remove(9)
